42da213ffa56adb0f06d2f18da3d2b1df7cad7fd
BUDAPEST\Melania_Bodza <Melania_Bodza@epam.com>
2021-05-25 21:31:54 +0200

    deleted comments

--- src/App.tsx
+++ src/App.tsx
@@ -9,83 +9,6 @@ import BrowserRouter from "./Routes";
 
 const { Suspense } = React;
 
-// const Router = createFarceRouter({
-//   historyProtocol: new BrowserProtocol(),
-//   historyMiddlewares: [queryMiddleware],
-//   routeConfig: makeRouteConfig(
-//     <Route
-//       path="/"
-//       Component={App}
-//       query={graphql`
-//         query AppQuery {
-//           allCustHists(last: 10, orderBy: ORDERID_DESC) {
-//             nodes {
-//               orderid
-//               prodId
-//             }
-//           }
-//           allCategories(last: 10) {
-//             nodes {
-//               categoryname
-//               category
-//               nodeId
-//             }
-//           }
-//         }
-//       `}
-//     >
-//       <Route path="customers">
-//         <Route
-//           Component={CustomerTable}
-//           query={graphql`
-//           query AppQuery {
-//             {
-//               allCustomers(first: 5) {
-//                 ...CustomerTable_customer
-//              }
-//         }`}
-//         />
-//       </Route>
-//     </Route>
-//   ),
-//   render: createRender({}),
-// });
-
-// ReactDOM.render(
-//   <Router resolver={new Resolver(RelayEnvironment)} />,
-//   document.getElementById("root")
-// );
-
-const Container = styled.div`
-  display: flex;
-  justify-content: space-around;
-  font-size: 16px;
-`;
-
-// const { Suspense } = React;
-
-// interface loadedQuery<T extends OperationType> {
-//   query: GraphQLTaggedNode;
-//   preloadedQuery: PreloadedQuery<T>;
-// }
-
-// // Define a query
-// export const myQuery = graphql`
-//   query AppQuery {
-//     allCustomers(first: 5) {
-//       ...CustomerTable_customer
-//     }
-//     allCustHists(last: 10, orderBy: ORDERID_DESC) {
-//       nodes {
-//         orderid
-//         prodId
-//       }
-//     }
-//     allCategories(last: 10) {
-//       ...CategoryTable_categories
-//     }
-//   }
-// `;
 // // Immediately load the query as our app starts. For a real app, we'd move this
 // // into our routing configuration, preloading data as we transition to new routes.
 // const preloadedQuery = loadQuery(RelayEnvironment, myQuery, {
@@ -99,75 +22,20 @@ const Container = styled.div`
 //   fallback.
 // - If the query failed, it throws the failure error. For simplicity we aren't
 //   handling the failure case here.
-function App() {
-  // const data: AppQueryResponse = usePreloadedQuery<AppQuery>(
-  //   myQuery,
-  //   props.preloadedQuery
-  // );
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div>
-          <ul>
-            <li>
-              <Link to="customer" activeClassName="active">
-                CustomerTable
-              </Link>
-            </li>
-            <li>
-              <Link to="category">CategoryTable</Link>
-            </li>
-          </ul>
-        </div>
-        <InputForm />
-        <Container>
-          <h3>Customer table</h3>
-          <h3>Category table</h3>
-        </Container>
-        {/* <Container>
-          {data?.allCustomers && <CustomerTable customer={data.allCustomers} />}
-          {data?.allCategories && (
-            <CategoryTable category={data.allCategories} />
-          )}
-        </Container> */}
-      </header>
-    </div>
-  );
-}
 
 // The above component needs to know how to access the Relay environment, and we
 // need to specify a fallback in case it suspends:
 // - <RelayEnvironmentProvider> tells child components how to talk to the current
 //   Relay Environment instance
 // - <Suspense> specifies a fallback in case a child suspends.
-function AppRoot() {
+function App() {
   return (
     <RelayEnvironmentProvider environment={RelayEnvironment}>
-      <Suspense fallback={"Loading..."}>
-        <div>
-          {/* <header className="App-header">
-            <div>
-              <ul>
-                <li>
-                  <Link to="customer" activeClassName="active">
-                    CustomerTable
-                  </Link>
-                </li>
-                <li>
-                  <Link to="category">CategoryTable</Link>
-                </li>
-              </ul>
-            </div>
-            <InputForm />
-          </header> */}
-          <div>
-            <BrowserRouter />
-          </div>
-        </div>
-      </Suspense>
+      <div>
+        <BrowserRouter />
+      </div>
     </RelayEnvironmentProvider>
   );
 }
 
-export default AppRoot;
+export default App;
