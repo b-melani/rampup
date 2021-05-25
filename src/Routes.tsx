576f818ef2959ab7abbaca3abdadc3070c38e46f
BUDAPEST\Melania_Bodza <Melania_Bodza@epam.com>
2021-05-25 21:19:00 +0200

    implemented Routes.tsx

---
+++ src/Routes.tsx
@@ -0,0 +1,55 @@
+import Route from "found/Route";
+import makeRouteConfig from "found/makeRouteConfig";
+import { createBrowserRouter, Redirect } from "found";
+import Link from "found/Link";
+import CategoryPage from "./CategoryPage";
+import CustomerPage from "./CustomerPage";
+import React from "react";
+
+const { Suspense } = React;
+
+const EmptyPage = () => {
+  return <span>Coming soon..</span>;
+};
+
+const PageLayout: React.FC = ({ children }) => {
+  return (
+    <>
+      <div>
+        <ul>
+          <li>
+            <Link to="/" exact>
+              home
+            </Link>
+          </li>
+          <li>
+            <Link to="/customer" activeClassName="active">
+              CustomerTable
+            </Link>
+          </li>
+          <li>
+            <Link to="/category" exact>
+              CategoryTable
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <Suspense fallback={"Loading..."}>{children}</Suspense>
+      </div>
+    </>
+  );
+};
+
+const BrowserRouter = createBrowserRouter({
+  routeConfig: makeRouteConfig(
+    <Route Component={PageLayout} path="/">
+      <Route Component={EmptyPage} />
+      <Route path="category" Component={CategoryPage} />
+      <Route path="customer" Component={CustomerPage} />
+      <Redirect from="*" to="/" />
+    </Route>
+  ),
+});
+
+export default BrowserRouter;
